cd2f944c25a74b672a15e710a938bd33c70a1f16
liangmi <121547620+liangmiQwQ@users.noreply.github.com>
2023-01-12 19:36:58 +0800

    主页更新
    主页更新页面，更新Footer和介绍

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <IndexPage></IndexPage>
+  </div>
+</template>
+
+<script>
+  import IndexPage from './pages/IndexPage.vue'
+  export default {
+    name: 'App',
+    components: {
+      IndexPage
+    }
+  }
+</script>
+
+<style>
+  #app {
+    width: 100%;
+    -webkit-font-smoothing: antialiased;
+    -moz-font-smoothing: grayscale;
+    height: 100%;
+  }
+</style>
---
+++ src/components/PageFooter.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="footer">
+    <h1>米卷</h1>
+    <p class="footerButton">一个免费且好用的国内问卷软件</p>
+    <p class="footerButton" style="cursor: pointer;" @click="goIcp">ICP备：</p>
+  </div>
+</template>
+<script>
+  export default{
+    name:"PageFooter",
+    methods:{
+      goIcp(){
+        window.location.href=""
+      }
+    }
+  }
+</script>
+<style>
+.footer{
+  padding: 50px 0 50px 0;
+  margin-top: 50px;
+  background: rgb(23,27,33);
+  text-align: center;
+  color: white;
+}
+.footerButton{
+  font-size: 17px;
+}
+</style>
